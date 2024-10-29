0f7ea1252496ffceb983b408ac8cbff35d743637
Iswar <73457704+iswarmondal@users.noreply.github.com>
2024-10-30 00:52:39 +0530

    Sign up and login
    - Setup tailwindcss and daisyUI
    - Connected to pocketbase instance
    - Created signup and login page

---
+++ frontend/src/routes/login/+page.svelte
@@ -0,0 +1,56 @@
+<script lang="ts">
+	import { pb } from '$lib/config/pocketbase.config';
+
+	let email: string = '';
+	let password: string = '';
+	let errorMessage: string = '';
+
+	async function signUp() {
+		try {
+			errorMessage = "";
+			const user = await pb.collection('users').authWithPassword(email.trim(), password.trim());
+			console.log('User loged in:', user);
+			console.log(pb.authStore.isValid)
+		} catch (error) {
+			if (error instanceof Error) {
+				errorMessage = error.message;
+			} else {
+				errorMessage = 'An unknown error occurred';
+			}
+			console.error('Error creating user:', error);
+		}
+	}
+    </script>
+
+<div class="flex justify-center items-center h-screen">
+	<div class="card w-96 shadow-xl">
+		<div class="card-body">
+			<h2 class="card-title text-center">Sign Up</h2>
+			<form on:submit|preventDefault={signUp} class="space-y-4">
+				<div class="form-control">
+					<label class="label" for="email">
+						<span class="label-text">Email</span>
+					</label>
+					<input type="email" bind:value={email} class="input input-bordered w-full" required />
+				</div>
+				<div class="form-control">
+					<label class="label" for="password">
+						<span class="label-text">Password</span>
+					</label>
+					<input
+						type="password"
+						bind:value={password}
+						class="input input-bordered w-full"
+						required
+					/>
+				</div>
+				{#if errorMessage}
+					<div class="text-red-500 text-sm">{errorMessage}</div>
+				{/if}
+				<div class="form-control mt-6">
+					<button type="submit" class="btn btn-primary w-full">Log in</button>
+				</div>
+			</form>
+		</div>
+	</div>
+</div>
